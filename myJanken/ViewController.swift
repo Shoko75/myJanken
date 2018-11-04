6dee2a7909a40dae198da6babe6b3b19b635d013
橋本 翔子 <shoko@ShokoAir.local>
2018-11-04 09:30:50 +0900

    Initial Commit

---
+++ myJanken/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  myJanken
+//
+//  Created by 橋本 翔子 on 2018/11/04.
+//  Copyright © 2018 橋本 翔子. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
